1f2ff16b87cc23f6811660233c7fd15e4c9f4bc4
Ayoub <erraghayayoub@gmail.com>
2020-05-29 18:21:22 +0100

    ginished

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks';
+import './App.css';
+import logo from './logo.png'
+import Launches from './components/Launches'
+import Launch from './components/Launch'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+const client = new ApolloClient({
+  uri: '/graphql'
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <img src={logo} alt="SpaceX" style={{ width: 300, display: 'block', margin: 'auto' }} />
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
